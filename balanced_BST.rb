195f263f7a613fc8c9ec1576243f6f58332dc990
Baron C <20404222+baron15cao@users.noreply.github.com>
2023-01-28 18:16:03 -0600

    Added find, insert and delete functions for BST

--- balanced_BST.rb
+++ balanced_BST.rb
@@ -33,16 +33,126 @@ class Tree
         return rt
     end
 
+    # def pretty_print created by TOP, not me
     def pretty_print(node = @root, prefix = '', is_left = true)
         pretty_print(node.rightNode, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.rightNode
         puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.nodeData}"
         pretty_print(node.leftNode, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.leftNode
-      end
+    end
+
+    def insert(val, rt = root)
+        if (val < rt.nodeData)
+            if (rt.leftNode != nil)
+                rt = rt.leftNode
+                insert(val, rt)
+            else
+                newNode = Node.new(val)
+                rt.leftNode = newNode
+                puts "Inserted #{val}, a left node."
+                return
+            end
+        elsif (val > rt.nodeData)
+            if (rt.rightNode != nil)
+                rt = rt.rightNode
+                insert(val, rt)
+            else
+                newNode = Node.new(val)
+                rt.rightNode = newNode
+                puts "Inserted #{val}, a right node"
+                return
+            end
+        else
+            puts "Found a duplicate. Invalid insert."
+        end
+    end
+
+    def find(val, rt = root, parent = nil, isLeft = true)
+        if (val == rt.nodeData)
+            puts "Found #{val}."
+            return rt, parent, isLeft
+        elsif (val < rt.nodeData)
+            if (rt.leftNode != nil)
+                parent = rt
+                rt = rt.leftNode
+                isLeft = true
+                find(val, rt, parent, isLeft)
+            else
+                puts "Not found."
+                return false, parent, isLeft
+            end
+        else
+            if (rt.rightNode != nil)
+                parent = rt
+                rt = rt.rightNode
+                isLeft = false
+                find(val, rt, parent, isLeft)
+            else
+                puts "Not found."
+                return false, parent, isLeft
+            end
+        end
+    end
+
+    def delete(val)
+        puts "Deleting #{val}."
+        found = self.find(val)
+        rt = found[0]
+        if (rt == false)
+            puts "Cannot delete."
+            return false
+        else
+            parent = found[1]
+            isLeft = found[2]
+            puts "Its parent is #{parent.nodeData}."
+            if (rt.leftNode == nil && rt.rightNode == nil)
+                puts "#{val} a terminal node."
+                if (isLeft)
+                    parent.leftNode = nil
+                else
+                    parent.rightNode = nil
+                end
+            elsif (rt.leftNode == nil)
+                puts "#{val} is a node with only right children, #{rt.rightNode.nodeData}."
+                puts "Connecting #{parent.nodeData} with #{rt.rightNode.nodeData}."
+                if (isLeft)
+                    parent.leftNode = rt.rightNode
+                else
+                    parent.rightNode = rt.rightNode
+                end
+            elsif (rt.rightNode == nil)
+                puts "#{val} is a node with only left children, #{rt.leftNode.nodeData}."
+                puts "Connecting #{parent.nodeData} with #{rt.leftNode.nodeData}."
+                if (isLeft)
+                    parent.leftNode = rt.leftNode
+                else
+                    parent.rightNode = rt.leftNode
+                end
+            # Now the fun one: Delete a node with children on both sides.
+            # Big idea: find the largest value in left node to replace it. Then delete that node.
+            # "That node" should fit in one of three cases above.
+            else
+                puts "#{val} is a node with two children, #{rt.leftNode.nodeData} and  #{rt.rightNode.nodeData}."
+                replacement = rt.leftNode
+                while (replacement.rightNode != nil)
+                    replacement = replacement.rightNode
+                end
+                replacementData = replacement.nodeData
+                puts "The value to replace that node is #{replacementData}. Saved."
+                self.delete(replacementData)
+                puts "Final step: replacing #{val} with #{replacementData}."
+                rt.nodeData = replacementData
+            end
+        end
+    end
 end
 
 
 #consoleTest
-testTree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+testTree = Tree.new([10, 40, 50, 100, 5, 25, 200, 500, 1000, 35, 115, 375, 225, 1250, 1300, 2000, 250, 550, 575, 750])
 puts testTree.pretty_print
-
-
+testTree.insert(400)
+puts testTree.pretty_print
+testTree.delete(115)
+puts testTree.pretty_print
+testTree.delete(550)
+puts testTree.pretty_print
